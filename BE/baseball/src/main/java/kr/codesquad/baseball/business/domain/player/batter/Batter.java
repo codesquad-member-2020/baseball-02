ca9be7479a2baaa8dc70e73e644fae458da0fcc8
IdionKim <ksundong@gmail.com>
2020-05-12 13:02:08 +0900

    [#31] feat: 도메인과 관련된 클래스 추가
    Batter와 Pitcher를 생성하였고, Data Access Object를 Repository로 정의하였습니다.

---
+++ BE/baseball/src/main/java/kr/codesquad/baseball/business/domain/player/batter/Batter.java
@@ -0,0 +1,16 @@
+package kr.codesquad.baseball.business.domain.player.batter;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@Builder
+@ToString
+public class Batter {
+
+    private final int id;
+    private final String name;
+    private final int battingNumber;
+    private final int hitRate;
+}
---
+++ BE/baseball/src/main/java/kr/codesquad/baseball/business/domain/player/pitcher/PitcherDao.java
@@ -0,0 +1,8 @@
+package kr.codesquad.baseball.business.domain.player.pitcher;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+@Slf4j
+@Repository
+public class PitcherDao {}
