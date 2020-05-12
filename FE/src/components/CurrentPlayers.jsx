dd49a79893291a48da2ab9fc6ad0636cb0c8fd94
Reese Kim <reesekimm@gmail.com>
2020-05-12 13:42:47 +0900

    [#21] feat: CurrentPlayer 컴포넌트 렌더링

--- FE/src/components/CurrentPlayers.jsx
+++ FE/src/components/CurrentPlayers.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { GAME_START } from "Utils/const";
+import { Container, PlayerType, PlayerInfo } from "Styles/CurrentPlayers";
 
 const CurrentPlayers = () => {
-	return <div>Current Players</div>;
+	const {
+		currentPlayers: { pitcher, batter },
+	} = GAME_START;
+
+	return (
+		<Container>
+			<PlayerType>투수</PlayerType>
+			<PlayerInfo>{`${pitcher.name} #${pitcher.number}`}</PlayerInfo>
+			<PlayerType>타자</PlayerType>
+			<PlayerInfo>{`${batter.name} #${batter.number} ${batter.atBat}타석 ${batter.hit}안타`}</PlayerInfo>
+		</Container>
+	);
 };
 
 export default CurrentPlayers;
---
+++ FE/src/styles/CurrentPlayers.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+	padding: 20px 15px;
+`;
+
+const PlayerType = styled.div`
+	font-size: 22px;
+	margin-bottom: 7px;
+`;
+
+const PlayerInfo = styled.div`
+	font-size: 17px;
+	margin-bottom: 20px;
+`;
+
+export { Container, PlayerType, PlayerInfo };
