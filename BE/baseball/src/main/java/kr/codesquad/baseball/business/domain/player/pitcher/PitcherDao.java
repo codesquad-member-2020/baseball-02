a73f7d8dfb2afbe173d1645e4ba0988433f6948c
IdionKim <ksundong@gmail.com>
2020-05-12 14:05:43 +0900

    [#31] feat: Batter와 Pitcher를 조회하는 기능 추가
    Team Id로 조회시 Batter는 List로 줍니다.
    RowMapper 가 중복되어서 Bean으로 등록했어요.

--- BE/baseball/src/main/java/kr/codesquad/baseball/business/domain/player/pitcher/PitcherDao.java
+++ BE/baseball/src/main/java/kr/codesquad/baseball/business/domain/player/pitcher/PitcherDao.java
@@ -1,8 +1,36 @@
 package kr.codesquad.baseball.business.domain.player.pitcher;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
 
 @Slf4j
 @Repository
-public class PitcherDao {}
+public class PitcherDao {
+
+    private static final String SELECT_PITCHER_BY_ID = "\nSELECT p.id, p.name\n  FROM pitcher p\n WHERE id = :id";
+    private static final String SELECT_PITCHER_BY_TEAM_ID = "\nSELECT p.id, p.name\n  FROM pitcher p\n WHERE team_id = :team_id";
+
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+    private final RowMapper<Pitcher> pitcherMapper;
+
+    public PitcherDao(DataSource dataSource, RowMapper<Pitcher> pitcherMapper) {
+        this.jdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+        this.pitcherMapper = pitcherMapper;
+    }
+
+    public Pitcher findById(int id) {
+        SqlParameterSource parameters = new MapSqlParameterSource("id", id);
+        return jdbcTemplate.queryForObject(SELECT_PITCHER_BY_ID, parameters, pitcherMapper);
+    }
+
+    public Pitcher findByTeamId(int teamId) {
+        SqlParameterSource parameters = new MapSqlParameterSource("team_id", teamId);
+        return jdbcTemplate.queryForObject(SELECT_PITCHER_BY_TEAM_ID, parameters, pitcherMapper);
+    }
+}
---
+++ BE/baseball/src/main/java/kr/codesquad/baseball/common/config/RowMapperConfig.java
@@ -0,0 +1,29 @@
+package kr.codesquad.baseball.common.config;
+
+import kr.codesquad.baseball.business.domain.player.batter.Batter;
+import kr.codesquad.baseball.business.domain.player.pitcher.Pitcher;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.RowMapper;
+
+@Configuration
+public class RowMapperConfig {
+
+    @Bean
+    public RowMapper<Batter> batterMapper() {
+        return (rs, rowNum) -> Batter.builder()
+                                     .id(rs.getInt("id"))
+                                     .name(rs.getString("name"))
+                                     .battingNumber(rs.getInt("batting_number"))
+                                     .hitRate(rs.getInt("bit_rate"))
+                                     .build();
+    }
+
+    @Bean
+    public RowMapper<Pitcher> pitcherMapper() {
+        return (rs, rowNum) -> Pitcher.builder()
+                                      .id(rs.getInt("id"))
+                                      .name(rs.getString("name"))
+                                      .build();
+    }
+}
