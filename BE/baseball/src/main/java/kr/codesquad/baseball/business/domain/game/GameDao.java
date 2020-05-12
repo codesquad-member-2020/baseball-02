4347e4b1f44ab43a09e62cfe992a6d8dc003d58f
IdionKim <ksundong@gmail.com>
2020-05-12 14:47:17 +0900

    [#31] feat: 게임 Domain 객체 생성 및 조회기능 추가

---
+++ BE/baseball/src/main/java/kr/codesquad/baseball/business/domain/game/GameDao.java
@@ -0,0 +1,34 @@
+package kr.codesquad.baseball.business.domain.game;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.time.ZoneId;
+
+@Slf4j
+@Repository
+public class GameDao {
+
+    private static final String SELECT_GAME_BY_ID = "\nSELECT g.id, g.play_date, g.home, g.away\n  FROM game g\n WHERE g.id = :id";
+
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+
+    public GameDao(DataSource dataSource) {
+        this.jdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+    }
+
+    public Game findById(int id) {
+        SqlParameterSource parameters = new MapSqlParameterSource("id", id);
+        return jdbcTemplate.queryForObject(SELECT_GAME_BY_ID, parameters, (rs, rowNum) ->
+                Game.builder()
+                    .id(rs.getInt("id"))
+                    .playDate(rs.getDate("play_date").toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime())
+                    .home(rs.getInt("home"))
+                    .away(rs.getInt("away"))
+                    .build());
+    }
+}
